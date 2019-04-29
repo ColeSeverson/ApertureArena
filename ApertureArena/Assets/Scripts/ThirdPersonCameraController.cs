a61e4ff4b9f5a5fc0db978495b11438af6229a8a
cseverso <cseverso@uoregon.edu>
2019-04-28 17:37:56 -0700

    Working on camera for player still. X movement works but Y movement messed it up. Will fix asap.

--- ApertureArena/Assets/Scripts/ThirdPersonCameraController.cs
+++ ApertureArena/Assets/Scripts/ThirdPersonCameraController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThirdPersonCameraController : MonoBehaviour
 {
     public GameObject character;
     public float mouseSens = 1.0f;
+    public Text debugText;
    // public float zoom;
 
     private Vector3 offset;
@@ -23,8 +25,23 @@ public class ThirdPersonCameraController : MonoBehaviour
         Vector3 mouseChange = lastMousePos - Input.mousePosition;
         lastMousePos = Input.mousePosition;
 
-        offset = Quaternion.AngleAxis(-mouseChange.x * mouseSens, Vector3.up) * offset;
-        transform.position = character.transform.position + offset;
+        Vector3 offset1 = Quaternion.AngleAxis(-mouseChange.x * mouseSens, Vector3.up) * offset;
+        Vector3 offset2 = new Vector3(0,0,0);
+        if (!((mouseChange.y < 0 && transform.position.y > 4) || (mouseChange.y > 0 && transform.position.y < -2)))
+            offset2 = Quaternion.AngleAxis(-mouseChange.y * mouseSens, Vector3.left) * offset;
+
+        transform.position = character.transform.position + (offset1 + offset2);
+        /*if (transform.position.y > 4)
+            transform.position = new Vector3(transform.position.x, 4, transform.position.z);
+        
+        if (transform.position.y < -2)
+            transform.position = new Vector3(transform.position.x, -2, transform.position.z);
+        */
         transform.LookAt(character.transform);
+
+        if (debugText)
+        {
+            debugText.text = transform.position.ToString();
+        }
     }
 }
