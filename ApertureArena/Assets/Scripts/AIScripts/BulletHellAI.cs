5a00f2bde5b758a2efaf24b90e1943edaeafdf6f
cseverso <cseverso@uoregon.edu>
2019-05-30 18:52:15 -0700

    Added pattern timings

--- ApertureArena/Assets/Scripts/AIScripts/BulletHellAI.cs
+++ ApertureArena/Assets/Scripts/AIScripts/BulletHellAI.cs
@@ -7,9 +7,14 @@ public class BulletHellAI : MonoBehaviour
     public Transform parent;
     public GameObject bullet;
     public Transform bulletSpawnArea; // an empty on the weapon
-    public float fireRate;
-    public float projectileSpeed = 15;
-    float temp = 0;
+
+    public float fireRate = 1f;
+    public float projectileSpeed = 15f;
+    
+    private float fireDelay = 0f;
+
+    //float temp = 0;
+
     private Pattern[] patterns;
 
     // Update is called once per frame
@@ -29,15 +34,14 @@ public class BulletHellAI : MonoBehaviour
     }
     public void Shoot()
     {
-        if (temp >= fireRate)
+        if (fireDelay <= 0)
         {
           float index = Random.Range(0f, patterns.Length);
-          patterns[(int)index].Execute(this, transform.forward,  projectileSpeed, fireRate);
-          temp = 0;
+          fireDelay = patterns[(int)index].Execute(this, transform.forward,  projectileSpeed, fireRate);
         }
         else
         {
-            temp += Time.deltaTime;
+            fireDelay -= Time.deltaTime;
         }
     }
 }
--- ApertureArena/Assets/Scripts/AIScripts/Pattern.cs
+++ ApertureArena/Assets/Scripts/AIScripts/Pattern.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public abstract class Pattern : MonoBehaviour
 {
-    public abstract void Execute(BulletHellAI ai, Vector3 forward, float bulletSpeed, float patternSpeed);
+    public abstract float Execute(BulletHellAI ai, Vector3 forward, float bulletSpeed, float patternSpeed);
 }
 public class Nova : Pattern
 {
     //This pattern loops once so pS doesn't effect it
-    public override void Execute(BulletHellAI ai, Vector3 forward,  float bS, float pS) {
+    public override float Execute(BulletHellAI ai, Vector3 forward,  float bS, float pS) {
       Quaternion rotation;
       for (int i = 0; i < 16; i++) {
-      
+
         Vector3 direction = Quaternion.AngleAxis(22.5f * i, Vector3.up) * forward;
 
         ai.fire(direction, bS);
       }
+
+      //this returns just the pattern speed because it is instantaneous
+      return pS * 1;
     }
 }
