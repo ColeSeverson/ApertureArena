320c565c084091d0aeacb43d47f026815b34f09e
cseverso <cseverso@uoregon.edu>
2019-05-30 18:40:39 -0700

    Added Patterns

--- ApertureArena/Assets/Scripts/AIScripts/BulletHellAI.cs
+++ ApertureArena/Assets/Scripts/AIScripts/BulletHellAI.cs
@@ -10,21 +10,30 @@ public class BulletHellAI : MonoBehaviour
     public float fireRate;
     public float projectileSpeed = 15;
     float temp = 0;
-   
+    private Pattern[] patterns;
+
     // Update is called once per frame
+    void Start(){
+      patterns = new Pattern[] {
+        new Nova()
+      };
+    }
     void Update()
     {
-        
-    }
 
+    }
+    public void fire(Vector3 direction, float bS) {
+      bullet = Instantiate(bullet, bulletSpawnArea.position, new Quaternion(90, 0, 0, 0));
+      bullet.GetComponent<Rigidbody>().AddForce(direction * bS);
+      Destroy(bullet, 10f);
+    }
     public void Shoot()
     {
         if (temp >= fireRate)
         {
-            bullet = Instantiate(bullet, bulletSpawnArea.position, new Quaternion(90, 0, 0, 0));
-            bullet.GetComponent<Rigidbody>().AddForce(parent.forward * projectileSpeed);
-            Destroy(bullet, 10f);
-            temp = 0;
+          float index = Random.Range(0f, patterns.Length);
+          patterns[(int)index].Execute(this, transform.forward,  projectileSpeed, fireRate);
+          temp = 0;
         }
         else
         {
---
+++ ApertureArena/Assets/Scripts/AIScripts/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pattern : MonoBehaviour
+{
+    public abstract void Execute(BulletHellAI ai, Vector3 forward, float bulletSpeed, float patternSpeed);
+}
+public class Nova : Pattern
+{
+    //This pattern loops once so pS doesn't effect it
+    public override void Execute(BulletHellAI ai, Vector3 forward,  float bS, float pS) {
+      Quaternion rotation;
+      for (int i = 0; i < 16; i++) {
+      
+        Vector3 direction = Quaternion.AngleAxis(22.5f * i, Vector3.up) * forward;
+
+        ai.fire(direction, bS);
+      }
+    }
+}
