47cd4fc1f015f46bb9fa75c6babbcd9830f2156e
Derrick Neal <dneal3@uoregon.edu>
2019-05-31 07:14:02 -0700

    ShotLevel added

--- ApertureArena/Assets/Scripts/AIScripts/Pattern.cs
+++ ApertureArena/Assets/Scripts/AIScripts/Pattern.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public abstract class Pattern : MonoBehaviour
 {
     public abstract float Execute(BulletHellAI ai, Vector3 forward, float bulletSpeed, float patternSpeed, float repetitions = 1);
+
+    protected int ShotLevel()
+    {
+        int[] levels = { 0, -1}; //if you want new shooting levels add them here
+        int randIndex = Random.Range(0, levels.Length);
+        return levels[randIndex];
+    }
 }
 public class Nova : Pattern
 {
     //This pattern loops once so pS doesn't effect it
     public override float Execute(BulletHellAI ai, Vector3 forward,  float bS, float pS, float reps = 1) {
-      Quaternion rotation;
+      int level = ShotLevel(); // randomly determine the shot level 
       for (int i = 0; i < 16; i++) {
 
         Vector3 direction = Quaternion.AngleAxis(22.5f * i, Vector3.up) * forward;
 
-        ai.fire(direction, bS);
+        ai.fire(direction, bS, level);
       }
 
       //this returns just the pattern speed because it is instantaneous
@@ -26,11 +33,12 @@ public class Spray : Pattern
 {
     public override float Execute(BulletHellAI ai, Vector3 forward, float bulletSpeed, float patternSpeed, float reps = 1)
     {
-        Quaternion rotation;
-        for (int i= 0; i<10; i++)
+        int level = ShotLevel();
+        for (int i = 0; i < 10; i++)
         {
             Vector3 direction = Quaternion.AngleAxis(6f * (i-5), Vector3.up) * forward;
-            ai.fire(direction, bulletSpeed);
+            ai.fire(direction, bulletSpeed, level);
+
         }
         return patternSpeed * 1;
     }
